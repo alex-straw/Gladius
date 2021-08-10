76d36fe5dc97d4c823dd934290a5f999b2f3e8f5
Alex Straw <as17163@bristol.ac.uk>
2021-08-10 15:13:32 +0000

    merges portfolios

--- tax_calculator/main.py
+++ tax_calculator/main.py
@@ -17,9 +17,9 @@ def main():
     results_path = r"C:\Users\alexa\Desktop\output_testing"  # Local output path for spreadsheets
 
     # Load trading data from excel spreadsheets
-    coinbase_df, coinbase_pro_df = load_files.load_trading_data(coinbase_path,coinbase_pro_path)
+    coinbase_df, coinbase_pro_df = load_files.load_trading_data(coinbase_path, coinbase_pro_path)
 
-    # Standardise portfolios for merge
+    # Standardise portfolios prior to data merge
     # FORMAT: | dd/mm/yyyy | unix | side | c1 name | c1 size | c2 name | c2 size | c2 size USD |
     coinbase_df = uniform_coinbase.organise_data(coinbase_df)
     coinbase_pro_df = uniform_coinbase_pro.organise_data(coinbase_pro_df)
@@ -28,6 +28,13 @@ def main():
     coinbase_df.to_csv(results_path + "\cb.csv")
     coinbase_pro_df.to_csv(results_path + "\cb_pro.csv")
 
+    # Merge portfolios sort chronologically
+    df = pd.concat([coinbase_df,coinbase_pro_df])
+    df = df.sort_values(by=["unix"])
+    df = df.reset_index(drop=True)
+
+    # Save merged portfolio to local directory
+    df.to_csv(results_path + "\portfolio.csv")
 
 if __name__ == '__main__':
     main()
