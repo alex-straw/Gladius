3bc767f7d58053fab52b9fe762635b61677cf666
Alex Straw <as17163@bristol.ac.uk>
2021-08-10 12:09:22 +0000

    reading in user trading data from outside repository - for eventual public release

--- tax_calculator/load_files.py
+++ tax_calculator/load_files.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
+import glob
+import os
+import pandas as pd
+
 """
 Created on Sunday August 09 12:34:00 2021
 Author: Alex Straw
 Description:
     Load files from a local folder
 """
+
+
+def load_trading_data(coinbase_path,coinbase_pro_path):
+    coinbase_df = pd.read_csv(coinbase_path)
+    coinbase_pro_df = pd.read_csv(coinbase_pro_path)
+
+    return coinbase_df,coinbase_pro_df
+
+
--- tax_calculator/main.py
+++ tax_calculator/main.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import os
+import re
+from datetime import datetime
+import load_files
+
+
+def main():
+    coinbase_path = r"C:\Users\alexa\Desktop\user_spreadsheets\cb.csv"  # r indicates raw string
+    coinbase_pro_path = r"C:\Users\alexa\Desktop\user_spreadsheets\cb_pro.csv"
+    coinbase_df,coinbase_pro_df = load_files.load_trading_data(coinbase_path,coinbase_pro_path)
+
+
+if __name__ == '__main__':
+    main()
