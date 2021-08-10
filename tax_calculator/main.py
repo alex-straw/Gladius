43bea7a154e4bc93fe5e361ee0a1db429ccc5b39
Alex Straw <as17163@bristol.ac.uk>
2021-08-10 17:52:48 +0000

    added timer - removed useless sort to improve processing time

--- tax_calculator/main.py
+++ tax_calculator/main.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 import re
 from datetime import datetime
+import time
 import load_files
 import uniform_coinbase
 import uniform_coinbase_pro
@@ -19,6 +20,7 @@ file_paths = {'coinbase': r"C:\Users\alexa\Desktop\user_spreadsheets\cb.csv",
 
 
 def main(file_paths):
+    t = time.time()
 
     # Load trading data from excel spreadsheets
     coinbase_df, coinbase_pro_df = load_files.load_trading_data(file_paths['coinbase'], file_paths['coinbase_pro'])
@@ -28,20 +30,19 @@ def main(file_paths):
     coinbase_df = uniform_coinbase.organise_data(coinbase_df)
     coinbase_pro_df = uniform_coinbase_pro.organise_data(coinbase_pro_df)
 
-    # Save updated portfolios to local directory
-    coinbase_df.to_csv(file_paths['results'] + "\cb.csv")
-    coinbase_pro_df.to_csv(file_paths['results'] + "\cb_pro.csv")
-
-    # Merge portfolios and sort chronologically by unix time
+    # Merge portfolios
     df = pd.concat([coinbase_df, coinbase_pro_df])
+
+    # Get total value of transaction and token prices in USD and sort chronologically by unix time
+    df = evaluate_in_fiat.get_prices(df,file_paths)
     df = df.sort_values(by=["unix"])
     df = df.reset_index(drop=True)
 
-    # Get total value of transaction and token prices in USD
-    df = evaluate_in_fiat.get_prices(df,file_paths)
-
     # Save merged portfolio to local directory
     df.to_csv(file_paths['results'] + "\portfolio.csv")
+
+    print(time.time() - t)
+
 
 
 if __name__ == '__main__':
