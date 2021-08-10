3644cb865cb97712714b7171f5d20f2e4f64c4ff
Alex Straw <as17163@bristol.ac.uk>
2021-08-10 18:53:40 +0000

    correctly returns a dictionary of crypto and USD trades

--- tax_calculator/crypto_portfolios.py
+++ tax_calculator/crypto_portfolios.py
@@ -23,14 +23,52 @@ def get_crypto_dataframes(df):
     return crypto_portfolios
 
 
-def make(df):
+def tidy_portfolio(df,crypto):
+    """
+    This function builds a new data frame that only contains:
+        -itself
+        -USD
+
+    This is done to prep for tax calculations later - as they are resolved in FIAT
+    """
+
+    columns_lhs = ['unix', 'side', 'c1 name', 'c1 size', 'c1 unit price USD']
+    columns_rhs = ['unix', 'side', 'c2 name', 'c2 size', 'c2 unit price USD']
+
+    #   This is necessary as some coins can act similarly to FIAT - as a medium of exchange (e.g BTC/ETH)
+    #   They are present in both columns: c1, and c2.
+    left_side_transactions = df[df['c1 name'] == crypto]
+    left_side_df = pd.DataFrame(left_side_transactions, columns=columns_lhs)
+
+    right_side_transactions = df[df['c2 name'] == crypto]
+    right_side_df = pd.DataFrame(right_side_transactions, columns=columns_rhs)
+
+    left_side_df = left_side_df.rename(columns={'unix': 'unix',
+                                                'side': 'side',
+                                                'c1 name': 'token',
+                                                'c1 unit price USD': 'Token price USD',
+                                                'c1 size': 'size'})
+
+    right_side_df = right_side_df.rename(columns={'unix': 'unix',
+                                                  'side': 'side',
+                                                  'c2 name': 'token',
+                                                  'c2 unit price USD': 'Token price USD',
+                                                  'c2 size': 'size'})
+
+    df = pd.concat([left_side_df, right_side_df])
+
+    df = df.sort_values(by="unix")
+    df = df.reset_index(drop=True)
+
+    df['current_holdings'] = df['size'].cumsum()
+
+    return df
+
+
+def make_fiat_crypto_portolios(df):
     crypto_portfolios = get_crypto_dataframes(df)
 
     for name in crypto_portfolios:
-        print(name)
+        crypto_portfolios[name] = tidy_portfolio(crypto_portfolios[name], name)
 
     return crypto_portfolios
-
-
-
-
