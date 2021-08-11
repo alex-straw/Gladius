4faa69f08d3fae8c2e2e19f671e7d6d7e17ba2ec
Alex Straw <64019579+alex-straw@users.noreply.github.com>
2021-08-11 14:29:08 +0100

    Update calculate_gains.py
    Explained reasoning for not splitting arrays into buy and sell dfs

--- tax_calculator/calculate_gains.py
+++ tax_calculator/calculate_gains.py
@@ -24,12 +24,17 @@ def prepare_spreadsheets(df):
     df['cost_basis_size'] = df['size']
     # Step 2: If the sign of a size column is negative (SELL), set the corresponding value in 'cost_basis_size' to 0 
     df.loc[np.sign(df['size']) == -1, 'cost_basis_size'] = 0
+    
+    # The cost basis can be calculated without splitting dfs into BUY / SELL orders
+    # This is because the 'cost_basis_size' will be lead to a 0 weighting in SELL cases, and correctly leave the cost basis 
+    # unchanged to that of the most recent 'BUY' order above - simplifying the process.
+    
                            
     # Important to note that on this spreadsheet the 'previous_holdings' column may refer to sell orders that are not present.
     # This is key as it allows for correct cost basis weightings.
                            
     # Recursive process that uses previous cost basis to calculate following cost basis.
-    # current_cost_basis = ((Token price USD * abs(size)) + (previous_holdings*previous_cost_basis))/current_holdings 
+    # current_cost_basis = ((Token price USD * (size above 0)) + (previous_holdings*previous_cost_basis))/current_holdings 
     
                            
                            
