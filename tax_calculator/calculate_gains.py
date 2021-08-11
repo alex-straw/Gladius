e4576a4edeaad9a0920dc8ac33f9bbee794769a6
Alex Straw <64019579+alex-straw@users.noreply.github.com>
2021-08-11 16:31:42 +0100

    Update calculate_gains.py

--- tax_calculator/calculate_gains.py
+++ tax_calculator/calculate_gains.py
@@ -19,27 +19,33 @@ def prepare_spreadsheets(df):
     # Only sell orders will directly lead to capital gains / losses
     
     # Strategy for optimising this calculation:
-   
-    # Step 1: Create an identical column
-    df['cost_basis_size'] = df['size']
-    # Step 2: If the sign of a size column is negative (SELL), set the corresponding value in 'cost_basis_size' to 0 
-    df.loc[np.sign(df['size']) == -1, 'cost_basis_size'] = 0
     
-    # The cost basis can be calculated without splitting dfs into BUY / SELL orders
+    # This strategy allows for the cost to be calculated without splitting dfs into BUY and SELL orders
     # This is because the 'cost_basis_size' will be lead to a 0 weighting in SELL cases, and correctly leave the cost basis 
     # unchanged to that of the most recent 'BUY' order above - simplifying the process.
     
-                           
-    # Important to note that on this spreadsheet the 'previous_holdings' column may refer to sell orders that are not present.
-    # This is key as it allows for correct cost basis weightings.
+    #| unix | date | token | Token price USD | size | current_holdings | previous_holdings | cost_basis_size |
+
+    #| unix | date | LINK  |       10        |  +50 |        50        |         0         |        50       |
+    #| unix | date | LINK  |       10        |  -10 |        50        |         0         |        0        |
+
+    # Step 1: Create an identical column
+    df['cur_cost_basis_size'] = df['size']
+    # Step 2: If the sign of a size column is negative (SELL), set the corresponding value in 'cost_basis_size' to 0 
+    df.loc[np.sign(df['size']) == -1, 'cost_basis_size'] = 0
+    
+    # Step 3: cur_trans_wt - current transaction weighting
+    # Step 3: prev_trans_wt - previous transaction weighting
+    df['cur_trans_wt'] = df['Token price USD'] * df['cur_cost_basis_size']
+    
+    df['
+    
+  
+                          
                            
     # Recursive process that uses previous cost basis to calculate following cost basis.
     # current_cost_basis = ((Token price USD * (size above 0)) + (previous_holdings*previous_cost_basis))/current_holdings 
-    
-                           
-                           
-    
-    sell_df = sell_df.append(np.sign([df[df['size']) == -1]])
+
                              
    """ REQUIRES TESTING: END """
     
