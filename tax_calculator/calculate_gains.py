1da6214f27e862135bba82073d85312d253798ec
Alex Straw <64019579+alex-straw@users.noreply.github.com>
2021-08-12 16:27:34 +0100

    Update calculate_gains.py
    Moved comments to improve readability

--- tax_calculator/calculate_gains.py
+++ tax_calculator/calculate_gains.py
@@ -29,31 +29,25 @@ def prepare_spreadsheets(df):
     buy_df = buy_df.append([np.sign(df[df['size']) == 1]])  
     # | x | unix | date | token | Token price USD | size | current_holdings | previous_holdings | cost_basis |
     # | 2 | unix | date | LINK  |       10        |  +50 |        50        |         0         |      0     |
-    # | 4 | unix | date | LINK  |       20        |  +25 |        50        |         25        |      0     |                       
+    # | 4 | unix | date | LINK  |       20        |  +25 |        50        |         25        |      0     |
+                            
+    sell_df = sell_df.append([np.sign(df[df['size']) == -1]])  
+    # | x | unix | date | token | Token price USD | size | current_holdings | previous_holdings | cost_basis |
+    # | 3 | unix | date | LINK  |       25        |  -25 |        25        |         50        |      0     |
     
-    # Vectorised prior to loop to minimise processing done within for loop                 
-    buy_df['price_x_size'] = buy_df['Token price USD'] * buy_df['size']
-    
-    # Set up the first cost_basis --> equal to the very first purchase price
-    buy_df.at[0,'cost_basis'] = buy_df['Token price USD'].values[0]                     
+    buy_df['price_x_size'] = buy_df['Token price USD'] * buy_df['size']  # Vectorised prior to loop to minimise processing done within for loop                 
+    buy_df.at[0,'cost_basis'] = buy_df['Token price USD'].values[0]  # Set up the first cost_basis --> equal to the very first purchase price                     
 
     # Time this section of code --> no straightforward vectorisation obvious
     for i in range(1, len(buy_df)):
         # cost basis = (Token price USD * trade size + previous cost basis USD * previous holdings) / current_holdings                   
         buy_df.loc[i, 'cost_basis'] = (buy_df.loc[i, 'price_x_size'] + buy_df.loc[i, 'previous_holdings'] * buy_df.loc[i-1, 'cost_basis'])/buy_df.loc[i, 'current_holdings']
-
-    # Drop temporary calculation column prior to merge                            
-    buy_df = df.drop('price_x_size', 1)
+                       
+    buy_df = df.drop('price_x_size', 1) # Drop temporary calculation column prior to merge            
 
     # Next step is to re-merge the buy_df and sell_df
     # Resort by date
-    # df['cost_basis'].fillna(method='ffill') --> this will set all sell cost basis values to first non zero cell above
-                            
-    sell_df = sell_df.append([np.sign(df[df['size']) == -1]])
-                              
-    # | x | unix | date | token | Token price USD | size | current_holdings | previous_holdings | cost_basis |
-    # | 3 | unix | date | LINK  |       25        |  -25 |        25        |         50        |      0     |
-                            
+    # df['cost_basis'].fillna(method='ffill') --> this will set all sell cost basis values to first non zero cell above                                   
                              
    """ REQUIRES TESTING: END """
     
