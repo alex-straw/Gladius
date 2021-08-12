500daeff466e0eabef16e46168005748dd8a7778
Alex Straw <64019579+alex-straw@users.noreply.github.com>
2021-08-12 12:34:56 +0100

    Update calculate_gains.py
    Different method due to issues

--- tax_calculator/calculate_gains.py
+++ tax_calculator/calculate_gains.py
@@ -18,29 +18,10 @@ def prepare_spreadsheets(df):
     # Only buy orders will directly affect the cost basis
     # Only sell orders will directly lead to capital gains / losses
     
-    # Strategy for optimising this calculation:
-    
-    # This strategy allows for the cost to be calculated without splitting dfs into BUY and SELL orders
-    # This is because the 'cost_basis_size' will be lead to a 0 weighting in SELL cases, and correctly leave the cost basis 
-    # unchanged to that of the most recent 'BUY' order above - simplifying the process.
-    
-    # | unix | date | token | Token price USD | size | current_holdings | previous_holdings | cost_basis_size | cur_trans_wt | cur_cost_basis    | prev_cost_basis |
-    # | unix | date | LINK  |       10        |  +50 |        50        |         0         |        50       |     500      |(500)+(0*0)/50= 10 |       0         |
-    # | unix | date | LINK  |       12        |  -10 |        40        |         50        |        0        |     0        |(40-50)*0 +        |       10        |
-    # | unix | date | LINK  |       15        |  +25 |        65        |         40        |        25       |     375      |(0)+(5             |       10        |
-    
-    # Step 1: Create an identical column
-    df['cur_cost_basis_size'] = df['size']
-    # Step 2: If the sign of a size column is negative (SELL), set the corresponding value in 'cost_basis_size' to 0 
-    df.loc[np.sign(df['size']) == -1, 'cost_basis_size'] = 0
-    
-    # Step 3: cur_trans_wt - current transaction weighting
-    # Step 3: prev_trans_wt - previous transaction weighting
-    df['cur_trans_wt'] = df['Token price USD'] * df['cur_cost_basis_size']
-             
-    # Recursive process that uses previous cost basis to calculate following cost basis.
-    # current_cost_basis = ((Token price USD * (size above 0)) + (previous_holdings*previous_cost_basis))/current_holdings 
-
+    # | unix | date | token | Token price USD | size | current_holdings | previous_holdings |
+    # | unix | date | LINK  |       10        |  +50 |        50        |         0         |
+    # | unix | date | LINK  |       25        |  -25 |        50        |         75        |
+    # | unix | date | LINK  |       20        |  +25 |        75        |         50        |
                              
    """ REQUIRES TESTING: END """
     
