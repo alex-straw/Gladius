6b13c6b9de80198c6c4c3e03817dedf559ff6d1b
Alex Straw <64019579+alex-straw@users.noreply.github.com>
2021-08-12 12:45:33 +0100

    Update calculate_gains.py
    Set up empty cost basis columns

--- tax_calculator/calculate_gains.py
+++ tax_calculator/calculate_gains.py
@@ -23,16 +23,25 @@ def prepare_spreadsheets(df):
     # | 3 | unix | date | LINK  |       25        |  -25 |        25        |         50        |
     # | 4 | unix | date | LINK  |       20        |  +25 |        50        |         25        |
     
+    df['prev_cost_basis'] = 0  # Set up empty cost basis columns
+    df['cur_cost_basis'] = 0
+    
+    # | x | unix | date | token | Token price USD | size | current_holdings | previous_holdings | prev_cost_basis | cur_cost_basis |
+    # | 2 | unix | date | LINK  |       10        |  +50 |        50        |         0         |        0        |        0       |
+    # | 3 | unix | date | LINK  |       25        |  -25 |        25        |         50        |        0        |        0       |
+    # | 4 | unix | date | LINK  |       20        |  +25 |        50        |         25        |        0        |        0       |
+    
     buy_df = buy_df.append([np.sign(df[df['size']) == 1]])
                             
-    # | x | unix | date | token | Token price USD | size | current_holdings | previous_holdings |
-    # | 2 | unix | date | LINK  |       10        |  +50 |        50        |         0         |
-    # | 4 | unix | date | LINK  |       20        |  +25 |        50        |         25        |
+    # | x | unix | date | token | Token price USD | size | current_holdings | previous_holdings | prev_cost_basis | cur_cost_basis |
+    # | 2 | unix | date | LINK  |       10        |  +50 |        50        |         0         |        0        |        0       |
+    # | 4 | unix | date | LINK  |       20        |  +25 |        50        |         25        |        0        |        0       |                       
+                                         
                             
     sell_df = sell_df.append([np.sign(df[df['size']) == -1]])
                               
-    # | x | unix | date | token | Token price USD | size | current_holdings | previous_holdings |
-    # | 3 | unix | date | LINK  |       25        |  -25 |        25        |         50        |
+    # | x | unix | date | token | Token price USD | size | current_holdings | previous_holdings | prev_cost_basis | cur_cost_basis |
+    # | 3 | unix | date | LINK  |       25        |  -25 |        25        |         50        |        0        |        0       |
                             
                              
    """ REQUIRES TESTING: END """
