9c38b6784b0cd9c475a7eb41a767e78209506c81
Alex Straw <as17163@bristol.ac.uk>
2021-08-18 16:41:26 +0000

    same day rule working correctly

--- tax_calculator/same_day_rule.py
+++ tax_calculator/same_day_rule.py
@@ -11,48 +11,67 @@ def same_day_pool(df, trade_type):
 
 
 def group_same_day(unique_days, df):
+    trade_type = []
+    quantity_token = []
+    residual_pool_value = []
+    net_same_day = []
 
     for day in unique_days:
         same_day_data = df.loc[(df['date'] == day)]
 
         quantity_acquired, pooled_value_acq = same_day_pool(same_day_data, trade_type='acquisition')
-
         quantity_disposed, pooled_value_disp = same_day_pool(same_day_data, trade_type='disposal')
-        quantity_disposed = quantity_disposed * -1  # Set values to be positive sign for consistency with HMRC
-        pooled_value_disp = pooled_value_disp * -1
 
-        residual = quantity_acquired - quantity_disposed
+        quantity_disposed = quantity_disposed * -1  # Set disposal values to be positive sign for consistency with HMRC
+        pooled_value_disp = pooled_value_disp * -1
 
         # Next stage is to assess if the day is a net disposal, acquisition, or resolved: (BUY=SELL)
         # Same day transactions will all be handled prior to pass of main algorithm
         # Planned data frame output --> each unique day will have a single transaction
         # | date | token_quantity | pooled_value | trade type | net same day |
 
-        print(residual)
+        residual = quantity_acquired - quantity_disposed
 
+        if residual > 0:
+            # if the residual is greater than 0 (net acquisition for the day):
+            trade_type.extend(['acquisition'])
+            quantity_token.extend([residual])
 
-        """
-    date = []
-    tok_grouped_by_day = []
-    value_grouped_by_day = []
-        tok_grouped_by_day.extend([token_acquired])
-        value_grouped_by_day.extend([value_acquired])
-        date.extend([day])
+            # Multiply cost basis of coins bought by the exact amount disposed
+            # Subtract the full 'pooled disposal amount' for the day
+            # This gives a net gain or loss according to the 'Same Day Rule' : TCGA1992/S105
 
-        tok_grouped_by_day.extend([token_disposed])
-        value_grouped_by_day.extend([value_disposed])
-        date.extend([day])  # Important to duplicate the day to split into acquired and disposed separately
+            net_same_day.extend([(quantity_disposed / quantity_acquired) * pooled_value_acq - pooled_value_disp])
 
-    trade_type = ['acquisition', 'disposal'] * len(unique_days)
+            # Calculate the value of the pooled assets remaining
+            residual_pool_value.extend([(residual / quantity_acquired) * pooled_value_acq])
 
-    same_day_df = {'date': date,
+        if residual < 0:
+            # if the residual is less than 0 (net disposal for the day):
+            trade_type.extend(['disposal'])
+            quantity_token.extend([residual * -1])  # Note minus sign
+
+            # Calculate the remaining pooled allowable costs for the disposal
+            residual_pool_value.extend([(residual / quantity_disposed) * pooled_value_disp])
+
+            # This gives a net gain or loss according to the 'Same Day Rule' : TCGA1992/S105
+
+            net_same_day.extend([pooled_value_acq - (quantity_acquired / quantity_disposed) * pooled_value_disp])
+
+        if residual == 0:
+            trade_type.extend(['resolved'])
+            quantity_token.extend([0])
+            residual_pool_value.extend([0])
+            net_same_day.extend([pooled_value_acq - pooled_value_disp])
+
+    same_day_df = {'date': unique_days,
                    'trade_type': trade_type,
-                   'token_quantity': tok_grouped_by_day,
-                   'value_usd': value_grouped_by_day,
+                   'quantity_token': quantity_token,
+                   'residual_pool_value': residual_pool_value,
+                   'net_same_day': net_same_day
                    }
 
     df = pd.DataFrame(data=same_day_df)
-    """
 
     return df
 
