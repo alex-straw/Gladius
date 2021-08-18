b4f434efabc1293c495dceab5139e6497d7663f0
Alex Straw <as17163@bristol.ac.uk>
2021-08-18 16:12:01 +0000

    commented plan for handling all same day transactions

--- tax_calculator/same_day_rule.py
+++ tax_calculator/same_day_rule.py
@@ -4,26 +4,41 @@ import pandas as pd
 
 def same_day_pool(df, trade_type):
     df_trade_type = df.loc[(df['trade_type'] == trade_type)]
-    token_amount = df_trade_type['size'].sum()
-    value = df_trade_type['value'].sum()
+    quantity = df_trade_type['size'].sum()
+    pooled_value = df_trade_type['value'].sum()
 
-    return token_amount, value
+    return quantity, pooled_value
 
 
 def group_same_day(unique_days, df):
-    date = []
-    tok_grouped_by_day = []
-    value_grouped_by_day = []
 
     for day in unique_days:
         same_day_data = df.loc[(df['date'] == day)]
 
-        token_acquired, value_acquired = same_day_pool(same_day_data, trade_type='acquisition')
+        quantity_acquired, pooled_value_acq = same_day_pool(same_day_data, trade_type='acquisition')
+
+        quantity_disposed, pooled_value_disp = same_day_pool(same_day_data, trade_type='disposal')
+        quantity_disposed = quantity_disposed * -1  # Set values to be positive sign for consistency with HMRC
+        pooled_value_disp = pooled_value_disp * -1
+
+        residual = quantity_acquired - quantity_disposed
+
+        # Next stage is to assess if the day is a net disposal, acquisition, or resolved: (BUY=SELL)
+        # Same day transactions will all be handled prior to pass of main algorithm
+        # Planned data frame output --> each unique day will have a single transaction
+        # | date | token_quantity | pooled_value | trade type | net same day |
+
+        print(residual)
+
+
+        """
+    date = []
+    tok_grouped_by_day = []
+    value_grouped_by_day = []
         tok_grouped_by_day.extend([token_acquired])
         value_grouped_by_day.extend([value_acquired])
         date.extend([day])
 
-        token_disposed, value_disposed = same_day_pool(same_day_data, trade_type='disposal')
         tok_grouped_by_day.extend([token_disposed])
         value_grouped_by_day.extend([value_disposed])
         date.extend([day])  # Important to duplicate the day to split into acquired and disposed separately
@@ -37,6 +52,7 @@ def group_same_day(unique_days, df):
                    }
 
     df = pd.DataFrame(data=same_day_df)
+    """
 
     return df
 
