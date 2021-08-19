e48b33cd8c2a9bd422394ab4b17d6ec3ec41bd7e
Alex Straw <as17163@bristol.ac.uk>
2021-08-19 11:28:46 +0000

    added comments explaining the purpose of the script and initial function 'final_pass'

--- tax_calculator/thirty_day_s104_rules.py
+++ tax_calculator/thirty_day_s104_rules.py
@@ -2,6 +2,20 @@ import pandas as pd
 import numpy as np
 
 
-def match_crypto(crypto_dict):
+def match_crypto(df):
+    return df
+
+
+def final_pass(crypto_dict):
+    """
+    This function will act as the starting point for the 30 day, and S104 pool rules.
+    Each row must be iterated over chronologically, and crypto matched from the first
+    disposals to the first acquisitions in the proceeding 30 days.
+
+    Each crypto asset will be handled separately prior to final capital gains summary.
+    """
+
+    for name in crypto_dict:
+        crypto_dict = match_crypto(crypto_dict[name])
 
     return crypto_dict
