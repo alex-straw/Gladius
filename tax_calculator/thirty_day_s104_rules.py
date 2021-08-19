01180d0b11181626540f42a486f362c2813ec080
Alex Straw <as17163@bristol.ac.uk>
2021-08-19 11:50:21 +0000

    now chronologically going over rows --> updating s104 pool with remaining unmatched shares and quantity

--- tax_calculator/thirty_day_s104_rules.py
+++ tax_calculator/thirty_day_s104_rules.py
@@ -3,6 +3,20 @@ import numpy as np
 
 
 def match_crypto(df):
+
+    s104_pool_allowable_costs = 0
+    s104_quantity_token = 0
+
+    for i in range(1, len(df)):
+        if df.loc[i, 'trade_type'] == 'acquisition':
+            # As orders are handled chronologically, if any acquisition has unmatched shares when
+            # it is reached, these can go directly into the s104 pool
+            s104_pool_allowable_costs += df.loc[i, 'residual_pool_value']
+            s104_quantity_token += df.loc[i, 'unmatched_tokens']
+        if df.loc[i, 'trade_type'] == 'disposal':
+            pass
+    print(s104_pool_allowable_costs)
+
     return df
 
 
@@ -16,6 +30,10 @@ def final_pass(crypto_dict):
     """
 
     for name in crypto_dict:
+        # Unmatched tokens will be gradually be matched and reduced.  Quantity token will remain unchanged.
+        crypto_dict[name]['unmatched_tokens'] = crypto_dict[name]['quantity_token']
+        crypto_dict[name]['residual_pool_value'] = abs(crypto_dict[name]['residual_pool_value'])
+
         crypto_dict[name] = match_crypto(crypto_dict[name])
 
     return crypto_dict
