31a0952be0c6182090b5df4f143b4114a8b73669
Alex Straw <as17163@bristol.ac.uk>
2021-09-08 15:58:22 +0000

    files are now loaded separately, gov example 6 has been recreated and tested - working correctly.  : https://www.gov.uk/hmrc-internal-manuals/cryptoassets-manual/crypto22256

--- tax_calculator/load_files.py
+++ tax_calculator/load_files.py
@@ -11,8 +11,18 @@ Description:
 """
 
 
-def load_trading_data(coinbase_path,coinbase_pro_path):
-    coinbase_df = pd.read_csv(coinbase_path,skiprows=7)
-    coinbase_pro_df = pd.read_csv(coinbase_pro_path)
+def coinbase(coinbase_path):
+    coinbase_df = pd.read_csv(coinbase_path, skiprows=7)
+    return coinbase_df
 
-    return coinbase_df,coinbase_pro_df
+
+def coinbase_pro(coinbase_pro_path):
+    coinbase_pro_df = pd.read_csv(coinbase_pro_path)
+    return coinbase_pro_df
+
+#
+# def load_trading_data(coinbase_path,coinbase_pro_path):
+#     coinbase_df = pd.read_csv(coinbase_path,skiprows=7)
+#     coinbase_pro_df = pd.read_csv(coinbase_pro_path)
+#
+#     return coinbase_df,coinbase_pro_df
